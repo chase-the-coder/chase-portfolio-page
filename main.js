9a29215bb13485ee39c2774a370ba60ca5434a76
Chase Lindsay <chase.lindsay1@gmail.com>
2020-03-29 17:02:59 -0400

    added banner

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 console.log('It works!');
 import { includeHTML } from './modules/includeHTML.js';
 includeHTML();
+import { initUpdateNavbarOnScroll } from './modules/navbarTransition.js';
+
+document.addEventListener('turbolinks:load', () => {
+	initUpdateNavbarOnScroll();
+});
