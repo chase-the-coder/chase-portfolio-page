b9b3c141826218374516d2fb0ad85357a73ecfa2
Chase Lindsay <chase.lindsay1@gmail.com>
2020-03-30 11:38:38 -0400

    got webpack working and installed typed.js

--- main.js
+++
@@ -1,8 +0,0 @@
-console.log('It works!');
-import { includeHTML } from './modules/includeHTML.js';
-includeHTML();
-import { initUpdateNavbarOnScroll } from './modules/navbarTransition.js';
-
-document.addEventListener('turbolinks:load', () => {
-	initUpdateNavbarOnScroll();
-});
