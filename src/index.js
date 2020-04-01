7193b0bd417caa15672589d9c727968b2be67441
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-01 16:30:12 -0400

    changing template and moving to new repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log('It works!');
 import { includeHTML } from './components/includeHTML.js';
 import { loadDynamicBannerText } from './components/banner.js';
+import _ from './components/navbarTransition';
 document.addEventListener('turbolinks:load', () => {
 	includeHTML();
 	loadDynamicBannerText();
